f2ef65c33338a82f3cfe087e2ca8c3c727f35d34
stephan lindauer <stephan.lindauer@gmail.com>
2015-01-02 13:41:47 +0100

    js folder

---
+++ js/main.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+	var footer = $("#footer");
+	footer.hide();
+	$("#foldbutton").click(function() {
+		var speed = 200;
+		if (footer.height() == 0) {
+			footer.show();
+			footer.animate({
+				height : "140px"
+			}, speed);
+		} else {
+			footer.animate({
+				height : "0px"
+			}, speed, function() {
+				footer.hide();
+			});
+		}
+	});
+});
