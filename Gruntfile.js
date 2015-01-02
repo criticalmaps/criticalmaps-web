1fe21c8e04b216d832d1251e90b5860988d4ed9a
stephan lindauer <stephan.lindauer@gmail.com>
2015-01-02 14:26:41 +0100

    gruntfile

---
+++ Gruntfile.js
@@ -0,0 +1,50 @@
+module.exports = function ( grunt ) {
+
+  require( 'load-grunt-tasks' )( grunt );
+
+  grunt.initConfig( {
+
+    pkg: grunt.file.readJSON( 'package.json' ),
+
+    copy: {
+      main: {
+       src: ['**/*'],
+        expand: true,
+        dot: true,
+        dest: 'upload'
+      }
+    },
+
+    clean: [
+      "upload/img/psd",
+    ],
+
+    smushit: {
+      group1: {
+        src: 'upload/'
+      }
+    },
+
+    'ftp-diff-deployer': {
+      options: {},
+      www: {
+        options: {
+          host: 'criticalmaps.net',
+          auth: {
+            username: process.env.FTP_USER,
+            password: process.env.FTP_PASSWORD
+          },
+          src: 'upload/',
+          dest: '/test'
+        }
+      }
+    }
+  } );
+
+  grunt.registerTask( 'default', [
+    'copy',
+    'clean',
+    'smushit',
+    'ftp-diff-deployer'
+  ] );
+};
