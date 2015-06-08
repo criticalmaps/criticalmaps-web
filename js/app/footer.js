64389c0d18a9f8ca174e03cb617e759ec1d85134
stephan lindauer <stephan.lindauer@gmail.com>
2015-06-08 13:34:28 +0200

    footer changes for impressum

--- js/app/footer.js
+++ js/app/footer.js
@@ -1,21 +1,5 @@
 define( ['jquery'], function ($) {
 
-  var footer = $( "#footer" );
-  footer.hide();
-  $( "#foldbutton" ).click( function () {
-    var speed = 200;
-    if ( footer.height() == 0 ) {
-      footer.show();
-      footer.animate( {
-        height: "140px"
-      }, speed );
-    } else {
-      footer.animate( {
-        height: "0px"
-      }, speed, function () {
-        footer.hide();
-      } );
-    }
-  } );
+
 
 } );
