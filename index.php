0443838cd9f3d5a551cf7f4232ab6487200f518d
stephan lindauer <stephan.lindauer@gmail.com>
2015-06-25 12:34:28 +0200

    linkage

--- index.php
+++ index.php
@@ -79,7 +79,7 @@
             <a href="http://www.pokuslabs.com/#contact">norman sander</a> (iphone) &
             <a href="http://www.stephanlindauer.de/contact">stephan lindauer</a> (android)
         </div>
-        <div class="impressum">impressum</div>
+        <div class="impressum"><a href="impressum.html" target="_blank">impressum</a> </div>
     </div>
 </div>
 
