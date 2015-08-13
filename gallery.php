3dd44af3d79a6efa784c122fda2784a4457a4e4c
stephan lindauer <stephan.lindauer@gmail.com>
2015-08-13 23:26:42 +0200

    gallery start

---
+++ gallery.php
@@ -0,0 +1,42 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8"/>
+</head>
+<body>
+
+<div class="gallerymap">
+
+</div>
+
+<script src="js/vendor/leaflet-src.js"/>
+<script src="js/vendor/jquery.js"/>
+
+<script type="text/javascript">
+    define( ["leaflet"], function ( L ) {
+
+        var currentMarkers = [];
+
+        var greenIcon = L.icon( {
+            iconUrl: '/img/bike.png',
+            iconSize: [20, 20],
+            iconAnchor: [10, 10]
+        } );
+
+        var zoom = 3;
+        var map = L.map( 'map', { zoomControl: false } );
+
+        map.setView( [52.468209, 13.425995], zoom );
+
+        L.tileLayer( 'http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        } ).addTo( map );
+
+        new L.Control.Zoom( { position: 'bottomleft' } ).addTo( map );
+
+    } );
+</script>
+
+
+</body>
+</html>
