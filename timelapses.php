374e700322229ed132b41d7ee98b9d62cf37b50a
stephan lindauer <stephan.lindauer@gmail.com>
2015-08-19 02:06:28 +0200

    hide timelapses for now

--- timelapses.php
+++ timelapses.php
@@ -3,21 +3,37 @@ require("partials/wrapper_head.php");
 ?>
 
 <style type="text/css">
-    #gallerymap, html, body {
-        padding: 0px;
+    html {
         margin: 0px;
         width: 100%;
-        height: 100%;
         background-color: #333333;
         left: 0;
         top: 0;
+        padding: 80px 0px 40px;
+    }
+
+    body{
+        overflow-y: auto;
+    }
+
+    #timelapses {
+        overflow-y: auto;
     }
 
 
 </style>
 
-<div id="gallerymap">
-    <iframe width="560" height="315" src="https://www.youtube.com/embed/td__IxD0HtU" frameborder="0" allowfullscreen></iframe>
+<div id="timelapses">
+
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/td__IxD0HtU" frameborder="0"
+            allowfullscreen></iframe>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/td__IxD0HtU" frameborder="0"
+            allowfullscreen></iframe>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/td__IxD0HtU" frameborder="0"
+            allowfullscreen></iframe>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/td__IxD0HtU" frameborder="0"
+            allowfullscreen></iframe>
+
 </div>
 
 
