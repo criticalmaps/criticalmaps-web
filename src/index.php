8610908f74717a8ab21b457c7debbf3bc437fbd9
zutrinken <xoxo@zutrinken.com>
2017-01-15 02:53:08 +0100

    removed future screenshots and simplified landing page

--- src/index.php
+++ src/index.php
@@ -1,71 +1,18 @@
 <?php require("wrapper_head.php"); ?>
 
-<div id="wrapper">
-	<div class="section" id="section-header">
-		<div class="inner content">
-			<img class="logo" alt="Critical Maps" src="/assets/images/logo.svg" width="800" height="600" />
-			<h1 class="tagline">Critical Maps is an open source Android and iOS App to organize the monthly critical mass movement around the world.</h1>
-			<div class="download">
-				<a class="button" href="https://itunes.apple.com/de/app/critical-mass-berlin/id918669647" target="_blank"><i class="icon-ios"></i> App Store</a>
-				<a class="button" href="https://play.google.com/store/apps/details?id=de.stephanlindauer.criticalmaps" target="_blank"><i class="icon-android"></i> Google Play</a>
-			</div>
+<div id="cover">
+	<div class="title">
+		<div class="logo-wrapper">
+			<figure class="logo"></figure>
+		</div>
+		<h1 class="tagline">Critical Maps is an open source Android and iOS App to organize the monthly critical mass movement around the world.</h1>
+		<div class="download">
+			<a class="button" href="https://itunes.apple.com/de/app/critical-mass-berlin/id918669647" target="_blank"><i class="icon-ios"></i> App Store</a>
+			<a class="button" href="https://play.google.com/store/apps/details?id=de.stephanlindauer.criticalmaps" target="_blank"><i class="icon-android"></i> Google Play</a>
+			<a class="button" href="https://github.com/criticalmaps" target="_blank"><i class="icon-github"></i> GitHub</a>
 		</div>
 	</div>
-	<div class="section" id="section-map">
-		<div class="inner content">
-			<div class="screenshot" data-type="prlx" data-speed="2"></div>
-			<div class="info">
-				<div class="text">
-					<h3>Map</h3>
-					<p>Share your anonymous position to stay together.</p>
-				</div>
-			</div>
-		</div>
-	</div>
-	<div class="section" id="section-info">
-		<div class="inner content">
-			<div class="screenshot" data-type="prlx" data-speed="2"></div>
-			<div class="info">
-				<div class="text">
-					<h3>Info</h3>
-					<p>Know always what to do and how to act with other participants.</p>
-				</div>
-			</div>
-		</div>
-	</div>
-	<div class="section" id="section-chat">
-		<div class="inner content">
-			<div class="screenshot" data-type="prlx" data-speed="2"></div>
-			<div class="info">
-				<div class="text">
-					<h3>Chat</h3>
-					<p>Spread important infos directly with the whole mass.</p>
-				</div>
-			</div>
-		</div>
-	</div>
-	<div class="section" id="section-twitter">
-		<div class="inner content">
-			<div class="screenshot" data-type="prlx" data-speed="2"></div>
-			<div class="info">
-				<div class="text">
-					<h3>Twitter</h3>
-					<p>See what others are experiencing during the mass.</p>
-				</div>
-			</div>
-		</div>
-	</div>
-	<div class="section" id="section-footer">
-		<div class="inner content">
-			<p class="tagline">Download the app via the App Store and Google Play or checkout the open source project on GitHub.</p>
-			<div class="download">
-				<a class="button" href="https://itunes.apple.com/de/app/critical-mass-berlin/id918669647" target="_blank"><i class="icon-ios"></i> App Store</a>
-				<a class="button" href="https://play.google.com/store/apps/details?id=de.stephanlindauer.criticalmaps" target="_blank"><i class="icon-android"></i> Google Play</a>
-				<a class="button" href="https://github.com/criticalmaps" target="_blank"><i class="icon-github"></i> GitHub</a>
-			</div>
-		</div>
-	</div>
+	<div class="cover"></div>
 </div>
-<div class="cover"></div>
 
 <?php require("wrapper_footer.php"); ?>
--- src/js/script.js
+++ src/js/script.js
@@ -20,12 +20,6 @@ $().ready( function () {
 	        return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
 	    }
 	};
-	
-	if(isMobile.iOS()) {
-		$('.screenshot').addClass('ios');
-	} else {
-		$('.screenshot').addClass('android');
-	}
 
 	$('#navigation ul a').each(function() {
 		var link = $(this);
@@ -39,54 +33,4 @@ $().ready( function () {
 	$('#nav-toggle').click(function() {
 		$('html').toggleClass('show-menu');
 	});
-	
-
-
-    var viewport = $(window);
-
-    function fading() {
-
-        $( '[data-type="prlx"]' ).each( function(){
-
-            var self = $( this );
-
-            var elementSpeed = self.attr( 'data-speed' );
-
-            var elementPosition = self.offset().top;
-
-            var windowPosition = viewport.scrollTop();
-
-            if( self.attr( 'data-pos' ) == 'top' ) {
-
-                windowPosition = windowPosition;
-
-            } else {
-
-                windowPosition = windowPosition + viewport.height() / 1.5;
-
-            }
-
-            var newPosition = ( windowPosition - elementPosition ) * ( elementSpeed / 33 );
-
-            self.css({
-                '-webkit-transform' : 'translate3d( 0, ' + newPosition + 'px, 0 )',
-                'transform' : 'translate3d( 0, ' + newPosition + 'px, 0 )'
-            });
-
-        });
-
-    }
-
-    fading();
-    viewport.on({
-        'scroll' : function() {
-            fading();
-        },
-        'resize' : function() {
-            fading();
-        },
-        'orientationchange' : function() {
-            fading();
-        }
-    });
 });
