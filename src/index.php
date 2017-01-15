bf4b40e0264068a303d81e3327799e6d3103a3e1
zutrinken <xoxo@zutrinken.com>
2017-01-15 03:14:32 +0100

    small improvments

--- src/index.php
+++ src/index.php
@@ -1,18 +1,23 @@
 <?php require("wrapper_head.php"); ?>
 
-<div id="cover">
-	<div class="title">
-		<div class="logo-wrapper">
-			<figure class="logo"></figure>
-		</div>
-		<h1 class="tagline">Critical Maps is an open source Android and iOS App to organize the monthly critical mass movement around the world.</h1>
-		<div class="download">
-			<a class="button" href="https://itunes.apple.com/de/app/critical-mass-berlin/id918669647" target="_blank"><i class="icon-ios"></i> App Store</a>
-			<a class="button" href="https://play.google.com/store/apps/details?id=de.stephanlindauer.criticalmaps" target="_blank"><i class="icon-android"></i> Google Play</a>
-			<a class="button" href="https://github.com/criticalmaps" target="_blank"><i class="icon-github"></i> GitHub</a>
+<div id="wrapper">
+	<div class="inner content">
+		<div id="cover">
+			<div class="title">
+				<div class="logo-wrapper">
+					<figure class="logo"></figure>
+				</div>
+				<h1 class="tagline">Critical Maps</h1>
+				<h2 class="subline">An open source Android and iOS App to organise the monthly critical mass movement around the world.</h2>
+				<div class="download">
+					<a class="button" href="https://itunes.apple.com/de/app/critical-mass-berlin/id918669647" target="_blank"><i class="icon-ios"></i> App Store</a>
+					<a class="button" href="https://play.google.com/store/apps/details?id=de.stephanlindauer.criticalmaps" target="_blank"><i class="icon-android"></i> Google Play</a>
+					<a class="button" href="https://github.com/criticalmaps" target="_blank"><i class="icon-github"></i> GitHub</a>
+				</div>
+			</div>
+			<div class="cover"></div>
 		</div>
 	</div>
-	<div class="cover"></div>
 </div>
 
 <?php require("wrapper_footer.php"); ?>
