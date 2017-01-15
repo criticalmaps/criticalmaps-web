9beaadc917bf24b6189771b1df2fe28f09106f86
zutrinken <xoxo@zutrinken.com>
2017-01-15 03:44:08 +0100

    new download buttons

--- src/index.php
+++ src/index.php
@@ -10,9 +10,8 @@
 				<h1 class="tagline">Critical Maps</h1>
 				<h2 class="subline">An open source Android and iOS App to organise the monthly critical mass movement around the world.</h2>
 				<div class="download">
-					<a class="button" href="https://itunes.apple.com/de/app/critical-mass-berlin/id918669647" target="_blank"><i class="icon-ios"></i> App Store</a>
-					<a class="button" href="https://play.google.com/store/apps/details?id=de.stephanlindauer.criticalmaps" target="_blank"><i class="icon-android"></i> Google Play</a>
-					<a class="button" href="https://github.com/criticalmaps" target="_blank"><i class="icon-github"></i> GitHub</a>
+					<a class="button button-android" href="https://play.google.com/store/apps/details?id=de.stephanlindauer.criticalmaps" target="_blank"><i class="icon-android"></i> Google Play</a>
+					<a class="button button-ios" href="https://itunes.apple.com/de/app/critical-mass-berlin/id918669647" target="_blank"><i class="icon-ios"></i> App Store</a>
 				</div>
 			</div>
 			<div class="cover"></div>
