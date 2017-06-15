0fc17c9e1fe701f0ebf06400f85a5347a0a42ae8
stephan lindauer <stephanlindauer@posteo.de>
2017-06-15 18:20:44 +0200

    Create utils.js

--- src/js/app/utils.js
+++ src/js/app/utils.js
@@ -1,6 +1,7 @@
 criticalMapsUtils = {
 
    convertCoordinateFormat : function ( oldFormat ) {
+    oldFormat = oldFormat.toString();
     var chars = oldFormat.split( '' );
     chars.splice( -6, 0, '.' );
     return chars.join( '' );
