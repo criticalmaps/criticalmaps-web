b2f7f6e013f9f19c5afce080fc9e7ff1600167a7
stephan lindauer <s.lindauer@rebuy.com>
2017-06-18 21:48:54 +0200

    fix gallery

--- src/gallery.php
+++ src/gallery.php
@@ -5,43 +5,43 @@
 <div id="gallerymap"></div>
 
 <script type="text/javascript">
-    $().ready( function () {
-	    var cameraIcon = L.icon( {
-	        iconUrl: '/assets/images/marker-photo.png',
-	        iconSize: [40, 40],
-	        iconAnchor: [20, 20]
-	    } );
+    $().ready(function () {
+        var cameraIcon = L.icon({
+            iconUrl: '/assets/images/marker-photo.png',
+            iconSize: [40, 40],
+            iconAnchor: [20, 20]
+        });
 
-	    var cameraMap = new L.map( 'gallerymap', { zoomControl: false } ).setView( [52.468209, 13.425995], 3 );
+        var cameraMap = new L.map('gallerymap', {zoomControl: false}).setView([52.468209, 13.425995], 3);
 
-	    L.tileLayer( 'http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-		    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-		} ).addTo( cameraMap );
+        L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(cameraMap);
 
-	    new L.Control.Zoom( { position: 'bottomleft' } ).addTo( cameraMap );
-	    new L.Hash( cameraMap );
+        new L.Control.Zoom({position: 'bottomleft'}).addTo(cameraMap);
+        new L.Hash(cameraMap);
 
-	    $.get( "https://api.criticalmaps.net/gallery/",
-	        function ( jsonString ) {
-	            var jsonObject = jQuery.parseJSON( jsonString );
-	            for ( var key in jsonObject ) {
-	                if ( jsonObject.hasOwnProperty( key ) ) {
-	                    var currentImageObject = jsonObject[key];
-	                    L.marker( [
-	                            criticalMapsUtils.convertCoordinateFormat( currentImageObject.latitude ),
-	                            criticalMapsUtils.convertCoordinateFormat( currentImageObject.longitude )],
-	                        { icon: cameraIcon }
-	                    )
-	                        .addTo( cameraMap )
-	                        .bindPopup(
-	                            '<a class="popuplink" target="_blank" href="http://api.criticalmaps.net/gallery/images/' + currentImageObject.imageId + '.jpg">' +
-	                            '<img class="popupimage" src="http://api.criticalmaps.net/gallery/images/' + currentImageObject.imageId + '_thumb.jpg">' +
-	                            '</a>' );
-	                }
-	            }
-	        }
-	    );
-    } );
+        $.get("http://api.criticalmaps.net/gallery/",
+            function (response) {
+                console.log(response)
+                for (var i = 0; i < response.length; i++) {
+                    var currentImageObject = response[i]
+                    console.log(currentImageObject);
+
+                    L.marker([
+                            criticalMapsUtils.convertCoordinateFormat(currentImageObject.latitude),
+                            criticalMapsUtils.convertCoordinateFormat(currentImageObject.longitude)],
+                        {icon: cameraIcon}
+                    )
+                        .addTo(cameraMap)
+                        .bindPopup(
+                            '<a class="popuplink" target="_blank" href="http://api.criticalmaps.net/gallery/image/' + currentImageObject.id + '">' +
+                            '<img class="popupimage" src="http://api.criticalmaps.net/gallery/thumbnail/' + currentImageObject.id + '">' +
+                            '</a>');
+                }
+            }
+        );
+    });
 </script>
 
 <?php require("wrapper_footer.php"); ?>
