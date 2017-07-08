4938c5a2e17d903b0bf54b822b1a2369433afdc4
stephan lindauer <s.lindauer@rebuy.com>
2017-07-09 00:35:28 +0200

    remove https again

--- src/map.php
+++ src/map.php
@@ -22,7 +22,7 @@
 
 	    var bikeMap = new L.map( 'map', { zoomControl: false } ).setView( [52.468209, 13.425995], 3 );
 
-	    L.tileLayer( 'https://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+	    L.tileLayer( 'http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
 	        attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 	    } ).addTo( bikeMap );
 
