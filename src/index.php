2f29a1b20db23ac672e122ba84feb7e4839912e4
zutrinken <xoxo@zutrinken.com>
2019-03-31 13:55:30 +0200

    cleanup frontpage

--- src/index.php
+++ src/index.php
@@ -7,16 +7,14 @@
 <div id="wrapper">
 	<div class="inner content">
 		<div class="title">
-			<h1 class="tagline"><figure class="logo">Critical Maps</figure></h1>
-			<h2 class="subline">An open source Android and iOS App to organise the monthly critical mass movement around the world.</h2>
+			<h1 class="tagline">Critical Maps</h1>
+			<p class="subline">An open source Android and iOS App to organise the monthly critical mass movement around the world.</p>
 			<div class="download">
-				<a class="button button-android" href="https://play.google.com/store/apps/details?id=de.stephanlindauer.criticalmaps" target="_blank"><i class="icon-android"></i> Google Play</a>
-				<a class="button button-ios" href="https://itunes.apple.com/app/critical-maps/id918669647" target="_blank"><i class="icon-ios"></i> App Store</a>
-				<a class="button button-github" href="https://github.com/criticalmaps"><i class="icon-github"></i> GitHub</a>
+				<a class="button button-ios" href="https://itunes.apple.com/app/critical-maps/id918669647" target="_blank"><span>App Store</span></a>
+				<a class="button button-android" href="https://play.google.com/store/apps/details?id=de.stephanlindauer.criticalmaps" target="_blank"><span>Google Play</span></a>
 			</div>
 		</div>
 	</div>
 </div>
-<div class="cover"></div>
 
 <?php require("wrapper_footer.php"); ?>
