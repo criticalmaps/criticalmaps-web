be8afd5d432ec86ab9dd207dcdbaf5724ea61a06
zutrinken <xoxo@zutrinken.com>
2019-04-01 11:26:39 +0200

    style front text

--- src/index.php
+++ src/index.php
@@ -6,12 +6,17 @@
 
 <div id="wrapper">
 	<div class="inner content">
-		<div class="title">
-			<h1 class="tagline">Critical Maps</h1>
-			<p class="subline">An open source Android and iOS App to organise the monthly critical mass movement around the world.</p>
-			<div class="download">
-				<a class="button button-ios" href="https://itunes.apple.com/app/critical-maps/id918669647" target="_blank"><span>App Store</span></a>
-				<a class="button button-android" href="https://play.google.com/store/apps/details?id=de.stephanlindauer.criticalmaps" target="_blank"><span>Google Play</span></a>
+		<div class="front">
+			<figure class="front-image">
+				<img src="/assets/images/app.jpg" width="430" height="608" alt="Critical Maps on iPhoneX" />
+			</figure>
+			<div class="front-text">
+				<h1 class="front-title">Critical Maps</h1>
+				<p class="front-description">An open source Android and iOS App to organise the monthly critical mass movement around the world.</p>
+				<div class="download">
+					<a class="button button-ios" href="https://itunes.apple.com/app/critical-maps/id918669647" target="_blank"><span>App Store</span></a>
+					<a class="button button-android" href="https://play.google.com/store/apps/details?id=de.stephanlindauer.criticalmaps" target="_blank"><span>Google Play</span></a>
+				</div>
 			</div>
 		</div>
 	</div>
