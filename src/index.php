90e9a59e86493af5fa05331ad016dbea85255611
zutrinken <xoxo@zutrinken.com>
2019-04-10 12:23:43 +0200

    add rel nopener to appstore links

--- src/index.php
+++ src/index.php
@@ -16,8 +16,8 @@
 				<h1 class="front-title">Critical Maps</h1>
 				<p class="front-description">An open source Android and iOS App to organise the monthly critical mass movement around the world.</p>
 				<div class="download">
-					<a class="button button-ios" href="https://itunes.apple.com/app/critical-maps/id918669647" target="_blank"><span>App Store</span></a>
-					<a class="button button-android" href="https://play.google.com/store/apps/details?id=de.stephanlindauer.criticalmaps" target="_blank"><span>Google Play</span></a>
+					<a class="button button-ios" href="https://itunes.apple.com/app/critical-maps/id918669647" target="_blank" rel="noopener"><span>App Store</span></a>
+					<a class="button button-android" href="https://play.google.com/store/apps/details?id=de.stephanlindauer.criticalmaps" target="_blank" rel="noopener"><span>Google Play</span></a>
 				</div>
 			</div>
 		</div>
